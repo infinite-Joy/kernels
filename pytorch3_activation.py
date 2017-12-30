cc6fc391de5df823fbf2a3df7ccd283605c8cb8d
Joydeep Bhattacharjee <joydeepubuntu@gmail.com>
2017-12-30 15:19:58 +0530

    pytorch activation functions

---
+++ pytorch3_activation.py
@@ -0,0 +1,54 @@
+
+# coding: utf-8
+
+# In[*]
+
+import torch
+from torch.nn import functional as F
+from torch.autograd import Variable
+import matplotlib.pyplot as plt
+
+
+# In[*]
+
+print('generate fake data')
+x = torch.linspace(-5, 5, 200)
+x = Variable(x)
+x_np = x.data.numpy()
+
+
+# In[*]
+
+print('following are popular activation functions.')
+y_relu = F.relu(x).data.numpy()
+y_sigmoid = F.sigmoid(x).data.numpy()
+y_tanh = F.tanh(x).data.numpy()
+y_softplus = F.softplus(x).data.numpy()
+
+
+# In[*]
+
+# plt to visualize these activation function
+plt.figure(1, figsize=(8, 6))
+plt.subplot(221)
+plt.plot(x_np, y_relu, c='red', label='relu')
+plt.ylim((-1, 5))
+plt.legend(loc='best')
+
+plt.subplot(222)
+plt.plot(x_np, y_sigmoid, c='red', label='sigmoid')
+plt.ylim((-0.2, 1.2))
+plt.legend(loc='best')
+
+plt.subplot(223)
+plt.plot(x_np, y_tanh, c='red', label='tanh')
+plt.ylim((-1.2, 1.2))
+plt.legend(loc='best')
+
+plt.subplot(224)
+plt.plot(x_np, y_softplus, c='red', label='softplus')
+plt.ylim((-0.2, 6))
+plt.legend(loc='best')
+
+plt.show()
+
